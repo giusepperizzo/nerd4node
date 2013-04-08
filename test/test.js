4efdbb8307c21f4ac6aef80c9607895501470e63
Yunjia Li <yl2@ecs.sotonacuk>
2013-04-08 15:35:10 +0100

    remove api_key in the test case

--- test/test.js
+++ test/test.js
@@ -5,10 +5,11 @@ var nerd = require('../lib/nerd4node');
 describe('nerd4node.js',function(){
 	describe('annotate',function(){
 		it("should return annotation results with no error",function(){
+			var API_KEY = "";
 			var text = 'Barack Hussein Obama II born August 4, 1961) is the 44th and current President of the United States, in office since 2009. He is the first African American to hold the office. Born in Honolulu';
 			nerd.annotate(
 					'http://nerd.eurecom.fr/api/', 
-		            "pfdr1b5dif9ind3i3rdcoave7oo4mo2n", 
+		            API_KEY, 
 		            'combined', 
 		            'text', 
 		            text, 
